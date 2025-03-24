d9a30e39c9eb435d96bde14e665d06817e6cb465
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-24 17:19:11 -0300

    :building_construction: Feat: Configuração do global.css / Configuração inicial do componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-function App() {
+import { Header } from './components/Header';
+ 
+ import './global.css';
+
+export function App() {
 
   return (
-    <h1>Projeto Feed</h1>
+    <div>
+      <Header />
+    </div>
   )
 }
-
-export default App
