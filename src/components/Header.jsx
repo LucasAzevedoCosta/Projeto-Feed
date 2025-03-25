6f49b3810cabe799e7c601f08c03057666f2dfe3
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-25 16:01:52 -0300

    :construction: Feat: Finalizando o componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
 
+import Logo from '../assets/Logo.jpg'
+
  export function Header() {
    return (
      <header className={styles.header}>
-       <strong>Lucas Feed</strong>
+       <img src={Logo} alt="Logotipo da Empresa" />
      </header>
    );
  }
