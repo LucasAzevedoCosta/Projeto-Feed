af45d7d4565e9cec05d405407da11efcf3ca23f5
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-26 13:29:33 -0300

    :construction: Feat: Criando o Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Header } from './components/Header';
- 
- import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
 
 export function App() {
 
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+         <Sidebar />
+         <main>
+         </main>
+       </div>
     </div>
   )
 }
