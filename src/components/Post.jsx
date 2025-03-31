4dec40f39052af29ecc7033782ecf35fc68353cb
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-31 17:43:32 -0300

    :construction: Feat: Deletando comentarios Parte 1

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,6 +36,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -58,10 +62,11 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
               <p>
+                key={line.content}
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -86,7 +91,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
