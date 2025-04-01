11b56998abe26c32074ef0d466e1e4f40b817664
Lucas Azevedo Costa <eozecos@gmail.com>
2025-04-01 16:01:33 -0300

    :recycle: Refactor: Refatorando o codigo para TypeScript

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -4,7 +4,13 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import { useState } from "react";
 import { Avatar } from "./Avatar";
 
-export function Comment({ content, onDeleteComment }) {
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
